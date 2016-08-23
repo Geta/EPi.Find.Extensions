ca19d85db98cb48cc26a7ff96926c86dffb4b6f1
Brian Weeteling <brianweeteling@gmail.com>
2016-08-23 14:47:45 +0200

    Add nuspec file (+1 squashed commits)
    Squashed commits:
    
    [0ee9b68] Initial commit of EPi.Find extensions

---
+++ src/EPi.Find.Extensions/TypeSearchExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq.Expressions;
+using EPiServer.Find;
+using EPiServer.Find.Api.Facets;
+using EPiServer.Find.Api.Querying;
+using EPiServer.Find.Helpers;
+using EPiServer.Find.Helpers.Reflection;
+
+namespace EPi.Find.Extensions
+{
+    public static class TypeSearchExtensions
+    {
+
+        /// <summary>
+        /// Add a filter conditionally, makes it easier to write a fluent query.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <param name="search">The search.</param>
+        /// <param name="condition">if set to <c>true</c> the filterExpression will be added.</param>
+        /// <param name="filterExpression">The filter expression.</param>
+        /// <returns></returns>
+        public static ITypeSearch<TSource> ConditionalFilter<TSource>(this ITypeSearch<TSource> search, bool condition, Expression<Func<TSource, Filter>> filterExpression)
+        {
+            if (!condition)
+            {
+                return search;
+            }
+
+            return search.Filter<TSource>(filterExpression);
+        }
+
+        /// <summary>
+        /// Gets term facets for a given field.
+        /// With parameter for setting result size
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <param name="search">The search.</param>
+        /// <param name="fieldSelector">The field selector.</param>
+        /// <param name="size">The number of facets to be returned.</param>
+        /// <returns></returns>
+        public static ITypeSearch<TSource> TermsFacetFor<TSource>(this ITypeSearch<TSource> search, Expression<Func<TSource, string>> fieldSelector, int? size)
+        {
+            return search.AddTermsFacetFor<TSource>((Expression)fieldSelector, (Action<TermsFacetRequest>)null, size);
+        }
+
+        /// <summary>
+        /// Gets term facets for a given field.
+        /// Adds ability to create facets for int fields
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <param name="search">The search.</param>
+        /// <param name="fieldSelector">The field selector.</param>
+        /// <param name="size">The number of facets to be returned.</param>
+        /// <returns></returns>
+        public static ITypeSearch<TSource> TermsFacetFor<TSource>(this ITypeSearch<TSource> search, Expression<Func<TSource, int>> fieldSelector, int? size)
+        {
+            return search.AddTermsFacetFor<TSource>((Expression)fieldSelector, (Action<TermsFacetRequest>)null, size);
+        }
+
+        private static ITypeSearch<TSource> AddTermsFacetFor<TSource>(this ITypeSearch<TSource> search, Expression fieldSelector, Action<TermsFacetRequest> facetRequestAction, int? size)
+        {
+            fieldSelector.ValidateNotNullArgument("fieldSelector");
+            string fieldPath = fieldSelector.GetFieldPath();
+            string fieldName = search.Client.Conventions.FieldNameConvention.GetFieldName(fieldSelector);
+            Action<TermsFacetRequest> action = facetRequestAction;
+            return search.TermsFacetFor<TSource>(fieldPath, (Action<TermsFacetRequest>)(x =>
+            {
+                x.Field = fieldName;
+                x.Size = size;
+                if (!action.IsNotNull())
+                    return;
+                action(x);
+            }));
+        }
+    }
+}
